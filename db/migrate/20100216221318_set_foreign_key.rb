b6d24d98091fc828e928e969863a9cc127e981ed
Ryan Taylor Long <ryan@rtlong.com>
2010-02-16 16:29:12 -0600

    Another typo

--- db/migrate/20100216221318_set_foreign_key.rb
+++ db/migrate/20100216221318_set_foreign_key.rb
@@ -1,9 +1,9 @@
 class SetForeignKey < ActiveRecord::Migration
   def self.up
-    add_foreign_key :contributions, :business_id, :business
+    add_foreign_key :contributions, :business_id, :businesses
   end
 
   def self.down
-    remove_foreign_key :contributions, :business
+    remove_foreign_key :contributions, :businesses
   end
 end
