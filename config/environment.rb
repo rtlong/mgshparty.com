20f2b69c540e09151abdc87b98640587883b7b58
Ryan Taylor Long <ryan@rtlong.com>
2011-03-31 02:02:55 -0500

    More of the spring cleaning... now working again on my laptop. It's fragile, and needs a lot of work to get it to production again.

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ require 'thread'
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.7' unless defined? RAILS_GEM_VERSION
 
 if File.exist?("tmp/restart.txt")
   LAST_UPDATE_TIME = File.new("tmp/restart.txt").mtime
@@ -42,7 +42,7 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem "formtastic"
+  config.gem "formtastic", :version => '1.2.3'
   config.gem "haml"
   config.gem "RedCloth"
 
